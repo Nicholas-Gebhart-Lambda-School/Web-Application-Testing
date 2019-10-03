518f70591ebd9f8aa4fa5e95d931c52617f59878
gebhartn <nicholas.gebhart@gmail.com>
2019-10-03 13:19:34 -0700

    Implement useButton

--- testing/src/components/Dashboard.jsx
+++ testing/src/components/Dashboard.jsx
@@ -1,9 +1,17 @@
+import { useButton } from "../hooks/useButton";
 import React from "react";
 
 export const Dashboard = () => {
+  const [strike, setStrikes, clickStrikes] = useButton(0);
+  const [ball, setBalls, clickBalls] = useButton(0);
+  const [foul, setFoul, clickFoul] = useButton(0);
+
   return (
     <>
-      <p>Hello Dashboard</p>
+      <button className="strikes" onClick={clickStrikes}>
+        Strikes
+      </button>
+      <p>{strike}</p>
     </>
   );
 };
